336453c634f10b5272ec51acac549ec4496a8d38
Naoya Ito <i.naoya@gmail.com>
2013-07-02 17:12:47 +0900

    Refactor and remove unused comments

--- app/controller/timeline_view_controller.rb
+++ app/controller/timeline_view_controller.rb
@@ -19,7 +19,7 @@ class TimelineViewController < UITableViewController
       self.navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(
         UIBarButtonSystemItemBookmarks,
         target:self,
-        action:'openProfile'
+        action:'open_profile'
       )
     end
 
@@ -37,29 +37,10 @@ class TimelineViewController < UITableViewController
         App.alert(res.error_message)
       end
     end
-
-    # Dispatch::Queue.concurrent.async do
-    #   json = nil
-    #   begin
-    #     json = JSONParser.parse_from_url(@feed_url)
-    #   rescue RuntimeError => e
-    #     presentError e.message
-    #   end
-
-    #   @bookmarks = json['bookmarks'].collect { |dict| Bookmark.new(dict) }
-
-    #   Dispatch::Queue.main.sync do
-    #     ## すでにviewが破棄されてる時がある(通信中にpopViewした場合など)ので nil チェック
-    #     unless self.view.nil?
-    #       self.view.reloadData
-    #     end
-    #   end
-    # end
   end
 
   def viewDidUnload
     super
-    # @pullToRefreshView = nil
   end
 
   def dealloc
@@ -109,7 +90,7 @@ class TimelineViewController < UITableViewController
     end
   end
 
-  def openProfile
+  def open_profile
     ProfileViewController.new.tap do |c|
       c.user    = @user
       c.as_mine = true
