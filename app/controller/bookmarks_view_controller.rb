02fe21346fe68d003ab52eab803cbbcaa45795eb
Naoya Ito <i.naoya@gmail.com>
2013-07-16 18:22:20 +0900

    Fix crash bug when load private-mode entry

--- app/controller/bookmarks_view_controller.rb
+++ app/controller/bookmarks_view_controller.rb
@@ -79,21 +79,25 @@ class BookmarksViewController < UIViewController
       if response.ok?
         json = BW::JSON.parse(response.body.to_str)
         ## FIXME: refactor with manager
-        @bookmarks = json['bookmarks'].collect do |dict|
-          Bookmark.new(
-            {
-              :title => entry.title,
-              :link  => entry.link,
-              :count => entry.count.to_s,
-              :user => {
-                :name => dict[:user]
-              },
-              :comment => dict[:comment],
-              :created_at => dict[:timestamp],
-              # 2005/02/10 20:55:55 => 2005-02-10T20:55:55+09:00
-              :datetime =>  dict[:timestamp].gsub(/\//, '-').gsub(/ /, 'T') + '+09:00'
-            }
-          )
+        if json['bookmarks'].present?
+          @bookmarks = json['bookmarks'].collect do |dict|
+            Bookmark.new(
+              {
+                :title => entry.title,
+                :link  => entry.link,
+                :count => entry.count.to_s,
+                :user => {
+                  :name => dict[:user]
+                },
+                :comment => dict[:comment],
+                :created_at => dict[:timestamp],
+                # 2005/02/10 20:55:55 => 2005-02-10T20:55:55+09:00
+                :datetime =>  dict[:timestamp].gsub(/\//, '-').gsub(/ /, 'T') + '+09:00'
+              }
+            )
+          end
+        else
+          App.alert("ブックマークが全てプライベートモード、もしくはコメント非表示設定のエントリーです")
         end
         unless @bookmarksTable.nil?
           @bookmarksTable.reloadData
