029c340da4de16e2f4d01bdbb5121bcbc91fcbe0
Naoya Ito <i.naoya@gmail.com>
2013-07-21 21:03:26 +0900

    Readabilityモードのフォントを明朝体に変更

--- app/util/readability.rb
+++ app/util/readability.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module Readability
   class Parser
     attr_accessor :api_token
@@ -31,6 +32,7 @@ body {
   padding-top: 20px;
   padding-bottom: 20px;
   word-wrap: break-word;
+  font-family: "Hiragino Mincho ProN", serif;
 }
 
 a {
@@ -43,14 +45,14 @@ img, video, iframe {
 
 h1 {
   font-size: 120%;
-  font-family: sans-serif;
+  // font-family: sans-serif;
   border-bottom: 1px solid #ccc;
   padding-bottom: 10px;
   margin-bottom: 0;
 }
 
 h2, h3, h4 {
-  font-family: sans-serif;
+  // font-family: sans-serif;
 }
 
 h2 {
