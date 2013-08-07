09e7c3947bb0d25fb7e892cdb40eb970ee93728b
Naoya Ito <i.naoya@gmail.com>
2013-08-07 21:11:51 +0900

    だいぶ力業っぽい解決策 #8

--- app/controller/hbfav2_navigation_controller.rb
+++ app/controller/hbfav2_navigation_controller.rb
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Hack: 戻るボタン長押しを検知したい
+# UIBarButtonItem には addGestureRecognizer できないので navigationBar 全体に引っかけて
+# 実際に押下されたらその地点がボタンの上かどうかを調べる
 class HBFav2NavigationController < UINavigationController
   def viewDidLoad
     super
@@ -7,14 +11,30 @@ class HBFav2NavigationController < UINavigationController
   end
 
   def gestureRecognizerShouldBegin(gestureRecognizer)
-    v = self.navigationBar.subviews.lastObject;
-    if (v.class.description.lowercaseString.rangeOfString("itembutton").location != NSNotFound)
-      p = gestureRecognizer.locationInView(v)
-      if (CGRectContainsPoint(v.bounds, p))
-        return true;
+    ## 戻るボタンらしきものを探す
+    v = self.navigationBar.subviews.find do |subview|
+      subview.class.description.lowercaseString.rangeOfString("itembutton").location != NSNotFound
+    end
+
+    ## そのボタンの枠内が押されていたら true
+    if v
+      point = gestureRecognizer.locationInView(v)
+      NSLog("[#{point.x}, #{point.y}]")
+      NSLog("[#{v.frame.origin.x}, #{v.frame.origin.y}], [#{v.frame.size.width}, #{v.frame.size.height}]")
+
+      # Quick Hack: ボタンは押せてるけど point が枠内に入ってないこともあるので枠を少し広げて計算させる
+      # (取得できるのがあくまで中心位置なので)
+      # 横幅は思いの他広い
+      rect = [ [v.frame.origin.x - 5.0 , v.frame.origin.y - 6.0 ], [ v.bounds.size.width + 40, v.bounds.size.height + 15 ] ]
+
+      # 上記理由により v.pointInside(point, withEvent:nil) ではダメ
+      if CGRectContainsPoint(rect, point)
+        return true
+      else
+        NSLog("おおっと")
       end
     end
-    return false
+    false
   end
 
   def backToRoot(recog)
