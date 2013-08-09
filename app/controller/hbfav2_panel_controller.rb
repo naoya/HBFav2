84cab238bf6f0dbb65a84a19a64a9a820f9b5d2a
Naoya Ito <i.naoya@gmail.com>
2013-08-09 16:55:03 +0900

    パフォーマンスが落ちるので角丸を抑止 #21

--- app/controller/hbfav2_panel_controller.rb
+++ app/controller/hbfav2_panel_controller.rb
@@ -5,18 +5,18 @@ class HBFav2PanelController < JASidePanelController
     @instance
   end
 
-  ## 上角だけ丸める
+  ## 上角だけ丸める : 有効にすると画面全体が Offscreen Rendered になって重い･･･
   def stylePanel(panel)
-    maskPath = UIBezierPath.bezierPathWithRoundedRect(
-      panel.bounds,
-      byRoundingCorners:(UIRectCornerTopLeft | UIRectCornerTopRight),
-      cornerRadii:CGSizeMake(5.0, 5.0)
-    )
+    # maskPath = UIBezierPath.bezierPathWithRoundedRect(
+    #   panel.bounds,
+    #   byRoundingCorners:(UIRectCornerTopLeft | UIRectCornerTopRight),
+    #   cornerRadii:CGSizeMake(5.0, 5.0)
+    # )
 
-    maskLayer = CAShapeLayer.alloc.init
-    maskLayer.frame = panel.bounds
-    maskLayer.path = maskPath.CGPath;
-    panel.layer.mask = maskLayer;
-    panel.clipsToBounds = true
+    # maskLayer = CAShapeLayer.alloc.init
+    # maskLayer.frame = panel.bounds
+    # maskLayer.path = maskPath.CGPath;
+    # panel.layer.mask = maskLayer
+    # panel.clipsToBounds = true
   end
 end
