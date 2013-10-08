b5bc49467dc082b7daa8b072ac6d0ef0434098d6
Naoya Ito <i.naoya@gmail.com>
2013-10-08 21:35:33 +0900

    Fixed readability button size

--- app/view/readability_bar_button_item.rb
+++ app/view/readability_bar_button_item.rb
@@ -2,9 +2,14 @@ class ReadabilityBarButtonItem < UIBarButtonItem
   def initWithTarget(target, action:action)
     self.initWithCustomView(
       UIButton.custom.tap do |btn|
-        btn.frame = [[0, 0], [38, 38]]
+        if UIDevice.currentDevice.ios7?
+          btn.frame = [[0, 0], [24, 24]]
+          btn.setImage(UIImage.imageNamed('readability-ios7'), forState: :normal.uicontrolstate)
+        else
+          btn.frame = [[0, 0], [38, 38]]
+          btn.setImage(UIImage.imageNamed('readability'), forState: :normal.uicontrolstate)
+        end
         btn.showsTouchWhenHighlighted = true
-        btn.setImage(UIImage.imageNamed('readability'), forState: :normal.uicontrolstate)
         btn.addTarget(target, action:action, forControlEvents:UIControlEventTouchUpInside)
       end
     )
