03e5c69dfb2aaba334e6b90d3fa865af4a05ed15
Naoya Ito <i.naoya@gmail.com>
2013-10-21 11:28:28 +0900

    Turn off ISBackGesture when iOS7

--- app/controller/hbfav2_back_gesture.rb
+++ app/controller/hbfav2_back_gesture.rb
@@ -1,6 +1,11 @@
 module HBFav2
   module BackGesture
     def configure_back_gesture
+      if UIDevice.currentDevice.ios7?
+        self.backGestureEnabled = false
+        return
+      end
+
       if home?
         self.backGestureEnabled = false
       else
