0d4f7ba41e26af6433464c5511f35f2347c1a75e
Naoya Ito <i.naoya@gmail.com>
2013-10-26 10:40:46 +0900

    UIDevice拡張が二つになってたので整理

--- app/cocoa/ui_device.rb
+++ app/cocoa/ui_device.rb
@@ -1,11 +1,12 @@
 class UIDevice
   def ios7?
+    return @is_ios7 unless @is_ios7.nil?
     version = UIDevice.currentDevice.systemVersion
     if (version.compare("7.0", options:NSNumericSearch) == NSOrderedSame or
         version.compare("7.0", options:NSNumericSearch) == NSOrderedDescending)
-      return true
+      return @is_ios7 = true
     end
-    return false
+    return @is_ios7 = false
   end
 
   def ios6?
