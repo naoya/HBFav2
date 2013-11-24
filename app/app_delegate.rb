eee8edd6d7c9229f13b1ef9ea1b5e04000a7e999
Naoya Ito <i.naoya@gmail.com>
2013-11-24 09:08:54 +0900

    Save url in pasteboard

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -182,13 +182,14 @@ class AppDelegate
 
   def watch_pasteboard
     pasteboard = UIPasteboard.generalPasteboard
-    url = pasteboard.URL
     if string = pasteboard.string
       if matches = string.match(%r{(https?://.*)})
+        url  = matches[1]
         user = ApplicationUser.sharedUser
         if user.last_url_in_pasteboard and user.last_url_in_pasteboard == url
         else
           user.last_url_in_pasteboard = url
+          user.save
           whenFoundURLInPasteboard(matches[1])
         end
       end
