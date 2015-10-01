fbbb6ef81d7c93b60d0a9b814aa544944615aeb7
Watson <watson1978@gmail.com>
2015-10-02 00:08:59 +0900

    [iPad] rotate View frame when rotate the device

--- app/controller/base/hbfav2_ui_view_controller.rb
+++ app/controller/base/hbfav2_ui_view_controller.rb
@@ -15,5 +15,17 @@ module HBFav2
       super
       self.navigationItem.backBarButtonItem = NavigationBackButton.create
     end
+
+    def willAnimateRotationToInterfaceOrientation(orientation, duration:duration)
+      # FIXME : Just rotate. need better way for iOS 7.
+      v = self.view.subviews.first
+      frame = v.frame
+      v.frame = CGRectMake(0, 0, frame.size.height, frame.size.width)
+    end
+
+    def viewWillTransitionToSize(size, withTransitionCoordinator:coordinator)
+      v = self.view.subviews.first
+      v.frame = CGRectMake(0, 0, size.width, size.height)
+    end
   end
 end
