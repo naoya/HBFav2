b9892d3a9793ac24f3ffe085a44a7faea8e10977
Naoya Ito <i.naoya@gmail.com>
2016-01-12 16:01:19 +0900

    Use new endpoint interface

--- app/model/bookmarks_manager.rb
+++ app/model/bookmarks_manager.rb
@@ -37,23 +37,16 @@ class BookmarksManager
   def sync!
     group = Dispatch::Group.new
     Dispatch::Queue.concurrent.async(group) do
-      @popular = get_bookmarks("http://b.hatena.ne.jp/api/viewer.popular_bookmarks?url=#{@url.escape_url}")      
+      @popular, _x = get_bookmarks("http://b.hatena.ne.jp/api/viewer.popular_bookmarks?url=#{@url.escape_url}")      
     end
 
     Dispatch::Queue.concurrent.async(group) do
-      @all = get_bookmarks(
-        "http://b.hatena.ne.jp/entry/jsonlite/?url=#{@url.escape_url}",
+      @all, @followers = get_bookmarks(
+        "http://comments.hbfav.com/#{ApplicationUser.sharedUser.hatena_id}?url=#{@url.escape_url}",
         {
-          :headers      => { "Cache-Control" => "no-cache" },
           :cache_policy => NSURLRequestReloadIgnoringLocalCacheData
         }
       )
-
-      ## 本当はこれも並列化したいが entry 情報が必要なので...
-      if @all.size > 0
-        ## all.first を渡すのは Bookmark オブジェクトにエントリ情報入ってるから
-        @followers = get_followers_bookmarks(ApplicationUser.sharedUser.hatena_id, @all.first)
-      end
     end
 
     App.shared.networkActivityIndicatorVisible = true
@@ -65,6 +58,7 @@ class BookmarksManager
 
   def get_bookmarks(url, options = {})
     bookmarks = []
+    followers = []
     http = HBFav2::HTTPClient.new
     http.headers      = options[:headers] if options[:headers]
     http.cache_policy = options[:cache_policy] if options[:cache_policy]
@@ -75,48 +69,14 @@ class BookmarksManager
           if entry and entry['bookmarks'].present?
             entry['bookmarks'].each { |bookmark| bookmarks.push(Bookmark.new_from_data(entry, bookmark)) }
           end
+          if entry and entry['followers'].present?
+            entry['followers'].each{|follower| followers.push(Bookmark.new_from_data(entry, follower)) }
+          end
         }
       end
       @responses.push(response)
     end
-    bookmarks
-  end
-
-  def get_followers_bookmarks(user, entry)
-    bookmarks = []
-    http = HBFav2::HTTPClient.new
-    http.get("http://comments.hbfav.com/#{user}?eid=#{entry.eid}") do |response|
-      if response.ok?
-        autorelease_pool {
-          data = BW::JSON.parse(response.content)
-          if data and data['comments'].present?
-            data['comments'].each { |rec|
-              # FIXME: Bookmark クラスへ
-              bookmark = Bookmark.new(
-                {
-                  :eid   => entry.eid,
-                  :title => entry.title,
-                  :link  => entry.link,
-                  :count => entry.count,
-                  :user => {
-                    :name => rec['user']
-                  },
-                  :comment => rec['comment'],
-
-                  # 2015/11/08 11:51:41
-                  :created_at => rec['timestamp'],
-
-                  # 2005-02-10T20:55:55+09:00
-                  :datetime => Bookmark.timestamp2dt(rec['timestamp'])
-                }
-              )
-              bookmarks.push(bookmark)
-            }
-          end
-        }
-      end
-    end
-    bookmarks
+    return bookmarks, followers
   end
 
   class Response
