ee4d8d198b50d1abadd0233fc3bfccc12fa30239
Watson <watson1978@gmail.com>
2016-03-17 20:12:43 +0900

    Expand tap region

--- app/cocoa/ui_view.rb
+++ app/cocoa/ui_view.rb
@@ -1,3 +1,16 @@
 class UIView
   include HBFav2::Rectangle
+
+  def resizeToFitWithSubviews
+    width = 0
+    height = 0
+
+    self.subviews.each do |v|
+      dw = v.frame.origin.x + v.frame.size.width
+      dh = v.frame.origin.y + v.frame.size.height
+      width  = dw if dw > width
+      height = dh if dh > height
+    end
+    self.frame = [self.frame.origin, [width, height]]
+  end
 end
